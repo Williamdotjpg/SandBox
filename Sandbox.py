86aa9f55b5d210ac5e41cc51138993244e4c1c9c
Will Holmes <78831828+Williamdotjpg@users.noreply.github.com>
2026-01-06 14:11:58 -0500

    Added variables  with User Input

--- Sandbox.py
+++ Sandbox.py
@@ -9,3 +9,12 @@ name = "Will Holmes"
 age = 15
 
 print("My name is",name,"is it is the year",year,"I am",age,"years old!")
+
+print("")
+
+year = input("What year is it: ")
+name = input("What is your name: ")
+age = input("How old are you: ")
+
+print("\nWelcome",name,"I see are",age,"years old in the year of",year + ".")
+      
